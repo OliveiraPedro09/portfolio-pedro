a6578ce3c418726d8c086c426270e3cb0b66f2d7
Pedro Martins de Oliveira <142221456+OliveiraPedro09@users.noreply.github.com>
2024-07-09 17:44:33 -0300

    Primeira versão do Portfolio digital
    Criação do portfolio digital usando tecnologias como JavaScript, React, HTML e CSS.
    
    O portfolio tem como objetivo o estudo das tecnologias utilizadas e divulgação de projetos.

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./intro.css";
+
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div className="intro-container">
+                <span className="ola">Olá,</span>
+                <span className="introText">Eu sou <span className="introName">Pedro</span><br />Software Developer</span>
+                
+            </div>
+            <img src="" alt="" className="bg" />
+        </section>
+    )
+}
+
+export default Intro;
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import contact from "../../assets/contact.svg";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+
+    return (
+        <nav className="navbar">
+            <img src={logo} alt="logo" className="logo" />
+            <div className="desktop-menu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">Sobre</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">Projetos</Link>
+            </div>
+            <button className="desktopMenuBtn">
+                <img src={contact} alt="" className="desktopMenuImg" />
+                Contact Me
+            </button>
+        </nav>
+    )
+
+}
+
+export default Navbar;
